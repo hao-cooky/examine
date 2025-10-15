f4f951b04e4558a10c81dd2b79d362da2da06b17
haonn <hao.nguyennhat97@gmail.com>
2025-10-15 19:03:32 +0700

    remove unsused

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 
-import 'primereact/resources/themes/lara-light-indigo/theme.css'; //theme
-import 'primereact/resources/primereact.min.css'; //core css
-import 'primeicons/primeicons.css'; //icons
-import 'primeflex/primeflex.css'; // flex
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 import {UsersTable} from "./components/UsersTable.tsx";
 
 function App() {
